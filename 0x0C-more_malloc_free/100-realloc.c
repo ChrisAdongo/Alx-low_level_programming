8b1e07d99c7ded5e71bd6dd85c5e62925acce6cc
ChrisAdongo <jakenyachris@gmail.com>
2023-03-16 05:15:26 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,7 +9,7 @@
  * 
  * Return: If new_size == old_size - ptr.
  *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
+ *         Otherwise - pointer to the reallocated memory block.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
